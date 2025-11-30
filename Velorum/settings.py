5db515b59f0a82605b24224e85e911d3eff0b7a3
Danny <a.ovalle@alumno.um.edu.ar>
2025-11-30 18:55:50 -0300

    Refactor email settings to use environment variables for credentials

--- Velorum/settings.py
+++ Velorum/settings.py
@@ -176,8 +176,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'alexpulguita04@gmail.com')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'fkza yjuv gqco culw')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'Velorum <noreply@velorum.com>'
 
 # Mercado Pago Configuration
